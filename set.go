b951dd3b1216a339a0b137ba9cfbd53fb5dc7448
Yota Toyama <raviqqe@gmail.com>
2017-10-30 00:36:54 +0900

    Cache sizes of sets

--- set.go
+++ set.go
@@ -2,23 +2,36 @@ package hamt
 
 // Set represents a set.
 type Set struct {
+	size int
 	hamt
 }
 
 // NewSet creates a new set.
 func NewSet() Set {
-	return Set{newHamt(0)}
+	return Set{0, newHamt(0)}
 }
 
 // Insert inserts a value into a set.
 func (s Set) Insert(e Entry) Set {
-	return Set{s.hamt.Insert(e).(hamt)}
+	size := s.size
+
+	if s.Find(e) == nil {
+		size++
+	}
+
+	return Set{size, s.hamt.Insert(e).(hamt)}
 }
 
 // Delete deletes a value from a set.
 func (s Set) Delete(e Entry) Set {
-	n, _ := s.hamt.Delete(e)
-	return Set{n.(hamt)}
+	n, b := s.hamt.Delete(e)
+	size := s.size
+
+	if b {
+		size--
+	}
+
+	return Set{size, n.(hamt)}
 }
 
 // Find finds a value in a set.
@@ -30,10 +43,16 @@ func (s Set) Find(e Entry) Entry {
 // This method is useful for iteration.
 func (s Set) FirstRest() (Entry, Set) {
 	e, n := s.hamt.FirstRest()
-	return e, Set{n.(hamt)}
+	size := s.size
+
+	if e != nil {
+		size--
+	}
+
+	return e, Set{size, n.(hamt)}
 }
 
 // Size returns a size of a set.
 func (s Set) Size() int {
-	return s.hamt.Size()
+	return s.size
 }
