d2beac495657ab8dfe0371e130837adcadd01539
Yota Toyama <raviqqe@gmail.com>
2017-10-29 18:16:11 +0900

    Install dependencies first

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,11 @@
-task :build do
+task :deps do
+  sh 'go get github.com/stretchr/testify/assert'
+end
+
+task build: :deps do
   sh 'go build'
 end
 
-task :test do
+task test: :deps do
   sh 'go test -cover'
 end
