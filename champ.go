4f70b9a51c9dfffd52fcc7c46842dbb138d893a8
Yota Toyama <raviqqe@gmail.com>
2017-10-29 11:47:29 +0900

    Add Delete, Find, and FirstRest methods

--- champ.go
+++ champ.go
@@ -12,3 +12,18 @@ func NewChamp() Champ {
 func (champ Champ) Insert(x interface{}) Champ {
 	return champ
 }
+
+// Delete deletes a key from a CHAMP.
+func (champ Champ) Delete(x interface{}) Champ {
+	return champ
+}
+
+// Find finds a key in a CHAMP.
+func (champ Champ) Find(x interface{}) interface{} {
+	return nil
+}
+
+// FirstRest returns a first key and a CHAMP without it.
+func (champ Champ) FirstRest() (interface{}, Champ) {
+	return nil, champ
+}
--- champ_test.go
+++ champ_test.go
@@ -10,3 +10,18 @@ func TestChampInsert(t *testing.T) {
 	champ := NewChamp()
 	champ.Insert(123)
 }
+
+func TestChampDelete(t *testing.T) {
+	champ := NewChamp()
+	champ.Delete(123)
+}
+
+func TestChampFind(t *testing.T) {
+	champ := NewChamp()
+	champ.Find(123)
+}
+
+func TestChampFirstRest(t *testing.T) {
+	champ := NewChamp()
+	champ.FirstRest()
+}
