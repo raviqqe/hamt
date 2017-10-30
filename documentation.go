51b07f52632b43d588e5dd5be056fc50b08119e8
Yota Toyama <raviqqe@gmail.com>
2017-10-30 13:40:25 +0900

    Add comment about collection operations

--- documentation.go
+++ documentation.go
@@ -1,3 +1,5 @@
 // Package hamt provides immutable collections types of maps (associative arrays)
 // and sets implemented by Hash-Array Mapped Tries (HAMT).
+// All operations of collections, such as insert and delete, are immutable and
+// creates new ones keeping original ones unmodified.
 package hamt
