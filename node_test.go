b21c20eaaf0a116fff48750620f4e6b9df06330f
Yota Toyama <raviqqe@gmail.com>
2017-10-30 10:29:39 +0900

    Fix typos

--- node_test.go
+++ node_test.go
@@ -2,10 +2,10 @@ package hamt
 
 import "testing"
 
-func TestHamtAsnode(t *testing.T) {
+func TestHamtAsNode(t *testing.T) {
 	t.Log(node(newHamt(0)))
 }
 
-func TestBucketAsnode(t *testing.T) {
+func TestBucketAsNode(t *testing.T) {
 	t.Log(node(newBucket()))
 }
