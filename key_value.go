b098cb396863f4bc0475438eda5cf9a569709dc7
Yota Toyama <raviqqe@gmail.com>
2017-10-30 10:28:19 +0900

    Add key-value pair type

---
+++ key_value.go
@@ -0,0 +1,22 @@
+package hamt
+
+type keyValue struct {
+	key   Entry
+	value interface{}
+}
+
+func newKeyValue(k Entry, v interface{}) keyValue {
+	return keyValue{k, v}
+}
+
+func (kv keyValue) Hash() int32 {
+	return kv.key.Hash()
+}
+
+func (kv keyValue) Equal(e Entry) bool {
+	if kv, ok := e.(keyValue); ok {
+		e = kv.key
+	}
+
+	return kv.key.Equal(e)
+}
---
+++ key_value_test.go
@@ -0,0 +1,29 @@
+package hamt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewKeyValue(t *testing.T) {
+	newKeyValue(EntryInt(42), "value")
+}
+
+func TestKeyValueAsEntry(t *testing.T) {
+	t.Log(Entry(newKeyValue(EntryInt(42), "value")))
+}
+
+func TestKeyValueHash(t *testing.T) {
+	newKeyValue(EntryInt(42), "value").Hash()
+}
+
+func TestKeyValueEqual(t *testing.T) {
+	k := EntryInt(42)
+	kv := newKeyValue(k, "value")
+
+	assert.True(t, kv.Equal(kv))
+	assert.True(t, kv.Equal(k))
+	assert.False(t, kv.Equal(newKeyValue(EntryInt(2049), "value")))
+	assert.False(t, kv.Equal(EntryInt(2049)))
+}
